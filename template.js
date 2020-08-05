292edb9d5e687a8c9b3f4cec7f937048e1bd6b96
MrRiaz <monsurrahmanriaz@gmail.com>
2020-08-06 00:57:53 +0600

    template & multipleLine

---
+++ template.js
@@ -0,0 +1,21 @@
+// old process for add string with space.
+// const firstName = 'Monsur';
+// const lastName = 'Rahman';
+// const fullName = firstName + ' ' + lastName;
+// console.log(fullName);
+
+
+
+//ES6 process for adding string with space.
+const firstName = 'Freelancer';
+const lastName = 'Riaz';
+const fullName = `${firstName} ${lastName} is a rich freelancer.`;
+console.log(fullName);      // akhane {} ar moddhe integer o bosiye calculate kora jabe.
+
+
+//ES6 process for multiple line..
+const multipleline = `i love you.
+i miss you,
+But now i don't need you
+Don't need you to stand belkoni at afternoon for me`;
+console.log(multipleline);
