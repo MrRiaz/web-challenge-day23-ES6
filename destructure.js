264759ab8e3a397b19e21b2336707cd67c1f982f
MrRiaz <monsurrahmanriaz@gmail.com>
2020-08-06 21:45:05 +0600

    done

--- destructure.js
+++ destructure.js
@@ -1,8 +1,25 @@
+//destructure object
 const person = {name: 'Jack William', age: 22, job: 'Facebooker', gfName: 'Ema Watson', address: 'Kochu khet', phone: '01717112211', friends: ['rahim', 'karim', 'sumon']}
 
 const { phone, name, friends} = person;
 console.log(name, phone, friends[1]);
 
+
+//complex object.
+
+const complexObject = {
+    name: 'Riaz',
+    info: {
+        address: 'mohakhali',
+        phone: '017111155544'
+    }
+}
+
+const {address} = complexObject.info;
+console.log(address);
+
+
+// array object
 
 const friends_name = ['Sakib Khan', 'Arman Khan', 'Salman Khan', 'Sharuk Khan', 'Rana Khan'];
 const [first, nextFriend, ...remainingFriends] = friends_name;
